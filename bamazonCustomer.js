20780b27ebcc4f4e340b68bf3b2dba4e39f99635
Planet15 <parmar0815@gmail.com>
2019-04-03 00:07:44 -0400

    got the results showing up in console, but the if else statements are not working properly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,46 +19,70 @@ connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     start();
+    console.log('\n -------- welcome ---------');
 });
 
 
-    
-    
+
+
 function start() {
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         console.table(results);
     });
-    inquirer
-    .prompt([
-        {
-        type:"input",
-        name:"purchase",
-        message:"Type in the ID of the product you would like to buy?",
-        },
 
-    {
-        type:"input",
-        name:"quantity",
-        message:"How many would you like to buy?"
-    },
-    {
-        type:"confirm",
-        name:"confirm",
-        message:"Please confirm your order?"
-    }
-])
-.then(function(answer){
-   var item_ID = answer.purchase;
-   var quantityAnswer = answer.quantity;
-//    var confirmAnswer = answer.quantity;
-});
-connection.query('SELECT * FROM `products` WHERE `id` = ?', [item_ID], function (error, results, fields) {
-    // error will be an Error if one occurred during the query
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
-    if (error) throw error;
-    console.log(results);
-    
-  });
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "purchase",
+                message: "Type in the ID of the product you would like to buy?",
+
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    } else {
+                        console.log('please choose a valid id');
+                        return false;
+                    }
+                }
+
+            },
+
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many would you like to buy?",
+
+                validate: function (value) {
+                    if (isNaN(value)) {
+                        console.log('please enter the quantity you would like');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+
+            },
+            {
+                type: "confirm",
+                name: "confirm",
+                message: "Please confirm your order?"
+            }
+        ])
+        .then(function (answer) {
+            var item_ID = answer.purchase;
+            var quantityAnswer = answer.quantity;
+            var confirmAnswer = answer.quantity;
+
+            connection.query('SELECT * FROM `products` WHERE `id` = ?', [item_ID, quantityAnswer, confirmAnswer], function (error, results) {
+                if (error) throw error;
+                // console.log(results);
+
+                
+
+            });
+
+
+        });
+
 }
