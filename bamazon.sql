3f081c18cf14d21e1867f5d7d40c14bd23740d51
Planet15 <parmar0815@gmail.com>
2019-04-02 22:52:57 -0400

    I am getting the table to show up in the terminal.

--- bamazon.sql
+++ bamazon.sql
@@ -1,19 +1,21 @@
 DROP DATABASE IF EXISTS bamazon_DB;
 CREATE database bamazon_DB;
 
+USE bamazon_DB;
+
 CREATE TABLE products (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(45) NOT NULL,
     price DECIMAL (10,2) NULL, 
     stock_quantity INT NULL,
-)
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("iphone", "mobile", 100.40, 5);
+VALUES ("iphone", "mobile", 100.40, 5),
+        ("iMac", "computer", 2000.30, 10),
+        ("shoes", "clothing", 115.30, 3);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity
-VALUES ("iMac", "computer", 2000.30, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("shoes", "clothing", 115.30, 3);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,64 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "bamazon_DB"
+    // Your password
+    password: "root",
+    database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+
+    
+    
+function start() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.table(results);
+    });
+    inquirer
+    .prompt([
+        {
+        type:"input",
+        name:"purchase",
+        message:"Type in the ID of the product you would like to buy?",
+        },
+
+    {
+        type:"input",
+        name:"quantity",
+        message:"How many would you like to buy?"
+    },
+    {
+        type:"confirm",
+        name:"confirm",
+        message:"Please confirm your order?"
+    }
+])
+.then(function(answer){
+   var item_ID = answer.purchase;
+   var quantityAnswer = answer.quantity;
+//    var confirmAnswer = answer.quantity;
+});
+connection.query('SELECT * FROM `products` WHERE `id` = ?', [item_ID], function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+    if (error) throw error;
+    console.log(results);
+    
   });
+}
